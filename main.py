aacc1b8c697c1d2459166cdc2aef8a7fcae39a98
Cinosaurio <167891497+Cinosaurio@users.noreply.github.com>
2024-10-04 14:45:30 -0600

    Added creata matrix and test matrix. NEEDS FIX

---
+++ main.py
@@ -0,0 +1,43 @@
+import random
+
+def create_matrix(rows,columns):
+    matrix=[]
+    ships = []
+    
+    while len(ships) < 5:
+        position = random.randint(0, rows * columns - 1)
+        
+        if ships.count(position) == 0:
+            ships.append(position)
+            
+    print(ships)        
+                
+    for i in range(rows):
+        rows=[]
+        for j in range(columns):
+            if ships.count(i * j) == 1:
+                rows.append(1)
+            else:
+                rows.append(0)
+        matrix.append(rows)
+    
+    
+    print(matrix)
+    
+    return matrix
+
+def test_matrix(matrix):
+    counter = 0
+    
+    for rows in matrix:
+        
+        for element in rows:
+            if element == 1:
+                counter += 1
+                
+    print(counter)
+
+
+
+test_matrix(create_matrix(8,8))
+      
